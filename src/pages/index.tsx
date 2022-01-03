65615f0cb95fc6e493de8d4cb3bae63e99d26cf5
dosadczuk <damian.osadczuk@gmail.com>
2022-01-03 18:39:57 +0100

    Add header with host input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,41 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [host, setHost] = useState<string>('')
+
+  useEffect(
+    () => {
+      console.log({ host })
+    },
+    [host],
+  )
+
   return (
-    <main>
-      <header>
-        <h1>Lorem ipsum dolor sit amet.</h1>
+    <div className="container mx-auto py-4 px-8 bg-white">
+      <header className="w-full mx-auto py-16 z-20">
+        <h1>
+          <div className="text-4xl text-black font-black">
+            Sprawdź czas odpowiedzi hosta
+          </div>
+          <div className="text-2xl text-indigo-500 font-bold">
+            oraz monitoruj jego stan
+          </div>
+        </h1>
+
+        <form className="flex gap-1 py-4">
+          <input type="url" onChange={({ target: { value } }) => { setHost(value) }}
+                 className="w-full rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder:text-gray-400"
+                 placeholder="np. wp.pl lub 192.168.0.1"
+          />
+
+          <button type="submit"
+                  className="px-6 bg-indigo-100 text-indigo-700 font-bold hover:bg-indigo-200 focus:ring focus:ring-indigo-500 rounded-md">
+            Sprawdź
+          </button>
+        </form>
       </header>
-    </main>
+    </div>
   )
 }
 
