ba23998086da7f7d333982b10fd54f4ac14a1235
dosadczuk <damian.osadczuk@gmail.com>
2022-01-03 16:43:31 +0100

    Init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <header>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </header>
+    </main>
+  )
+}
+
+export default Home
